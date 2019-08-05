ecb6c2f52cb6f437590755ae54f63e54804fa6aa
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 02:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 function shout(string) { return string.toUpperCase ( ) }
 function whisper(string) { return string.toLowerCase ( ) }
-function logShout(string) {const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
+function logShout(string) {
