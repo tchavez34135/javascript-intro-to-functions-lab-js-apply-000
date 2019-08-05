77328f3c4fbb3bd2714baf76506ef90115b2f849
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 02:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function shout(string) {return string.toUpperCase ()}
 function whisper(string) {return string.toLowerCase ()}
-
+function logShout(string) { return string.toUpperCase () } 
+console.log(func(logShout)); 
