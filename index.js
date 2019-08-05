9c9940d289925d827da3e107b2ec2f91812ed7d1
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 03:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function shout(string) {return string.toUpperCase ()}
 function whisper(string) {return string.toLowerCase ()}
 function logShout(string) {console.log ('HELLO');}
 function logWhisper(string) {console.log ('hello');}
-function sayHiToGrandma (aWord) {
+function sayHiToGrandma (string) {
   var cantUnswer = "I can't hear you!";
   var yesUnswer = "YES INDEED!";
   var lovUnswer = "I love you, too.";
-  if (aWord.toLowerCase(aWord) === aWord) {
+  if (string.toLowerCase(aWord) === aWord) {
     return cantUnswer;
   }
-  else if (aWord.toUpperCase(aWord) === aWord) {
+  else if (string.toUpperCase(aWord) === aWord) {
     return yesUnswer;
   }
   else if ("I love you, Grandma." === aWord) {
