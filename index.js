130e0532334234237d9868d2fab0a2ddf2ff05f8
practical-designer-1164 <tchavez34135@comcast.net>
2019-08-05 03:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string) {return string.toUpperCase ()}
 function whisper(string) {return string.toLowerCase ()}
 function logShout(string) {console.log ('HELLO');}
 function logWhisper(string) {console.log ('hello');}
-function sayHiToGrandma(string) (string.toLowerCase() === string) {
+function sayHiToGrandma(string) {if (string.toLowerCase() === string) {
 return 'I can’t hear you!' ;}
   if (string.toUpperCase() === string ) 
   {return 'YES INDEED!'} if (string === `I love you, Grandma.` ) {return 'I love you, too.'}}
